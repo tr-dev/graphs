00ac3e7f370269dfcee35d2dcbca89976256e00e
tr mcalexander <tr@mcalexander.me>
2017-03-06 08:16:25 -0500

    Find all of the shortest paths

--- graphs.js
+++ graphs.js
@@ -3,4 +3,12 @@ const BFS = require('./lib/BFSClass');
 
 let b = new BFS(10);
 console.log(b + '')
+let verts = b.getVerts();
+(verts || []).forEach((vert) => {
+  (verts || []).forEach((childVert) => {
+    if(vert !== childVert) {
+      b.shortestPath(vert, childVert);
+    }
+  })
+})
 b.shortestPath('2', '5');
